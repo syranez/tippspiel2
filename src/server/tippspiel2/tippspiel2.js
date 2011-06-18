f06b3ec735c66d226632f992ea18cb0f3ebdb2fc
syranez <syranez@minaga-church.de>
2011-06-18 21:02:40 +0200

    routes

--- src/server/tippspiel2/tippspiel2.js
+++ src/server/tippspiel2/tippspiel2.js
@@ -1,3 +1,12 @@
-exports.tippspiel2 = function () {
-    console.log("Tippspiel2.");
+var connect = require("connect");
+var routes  = require("./router/routes");
+
+// Serverport
+var port = 9876;
+
+exports.tippspiel2 = function tippspiel2 () {
+    var server = connect.createServer();
+    server.use(routes.routes)
+    server.listen(port);
+    console.log('Connect server listening on port ' + port);
 }
